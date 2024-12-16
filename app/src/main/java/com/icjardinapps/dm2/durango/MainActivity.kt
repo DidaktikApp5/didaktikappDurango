7ae895759ed5650240a84ad24ea943e1431bd627
Julio Gonzalez <juliogtrenard@gmail.com>
2024-12-16 11:32:48 +0100

    ADD splash screen

--- app/src/main/java/com/icjardinapps/dm2/durango/MainActivity.kt
+++ app/src/main/java/com/icjardinapps/dm2/durango/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.icjardinapps.dm2.durango
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash: SplashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        screenSplash.setKeepOnScreenCondition { false }
     }
 }
