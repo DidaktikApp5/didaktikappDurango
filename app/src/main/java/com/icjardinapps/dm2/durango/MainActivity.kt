ab8d33a2347802d598634718515a6ec53c55f118
Julio Gonzalez <juliogtrenard@gmail.com>
2024-12-16 12:00:32 +0100

    ADD Menu principal

--- app/src/main/java/com/icjardinapps/dm2/durango/MainActivity.kt
+++ app/src/main/java/com/icjardinapps/dm2/durango/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.icjardinapps.dm2.durango
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
